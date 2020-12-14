1d5595ad7a99c88db66b3416432cfe078e7fa321
ChristianStefaniw <christian.stefaniw@student.tdsb.on.ca>
2020-12-14 14:09:19 -0500

    I think this worked

--- js/animate.js
+++ js/animate.js
@@ -1,8 +1,26 @@
-window.onload(anime({
-    targets: 'div.fly-up',
-    translateY:[
-        {value: $(window).height(), duration: 0},
-        {value: 0, duration: 1000}
-    ],
-    elasticity: 10,
-}));
+var imgs = document.images,
+    len = imgs.length,
+    counter = 0;
+
+[].forEach.call( imgs, function( img ) {
+    if(img.complete)
+        incrementCounter();
+    else
+        img.addEventListener( 'load', incrementCounter, false );
+} );
+
+function incrementCounter() {
+    counter++;
+    if ( counter === len ) {
+        anime({
+            targets: 'div.fly-up',
+            translateY:[
+                {value: $(window).height(), duration: 0},
+                {value: 0, duration: 1000}
+            ],
+            elasticity: 10,
+        });
+    }
+}
+
+
