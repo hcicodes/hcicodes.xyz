e050d2213068beface7652ac4f0e90247d6016de
ChristianStefaniw <christian.stefaniw@student.tdsb.on.ca>
2020-12-14 14:07:15 -0500

    image loading fix

--- js/animate.js
+++ js/animate.js
@@ -1,8 +1,8 @@
-anime({
+window.onload(anime({
     targets: 'div.fly-up',
     translateY:[
         {value: $(window).height(), duration: 0},
         {value: 0, duration: 1000}
     ],
     elasticity: 10,
-});
+}));
