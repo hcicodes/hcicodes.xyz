803b78ce0b27a9b016cb4c555763bd0d4ef17b78
ChristianStefaniw <christian.stefaniw@student.tdsb.on.ca>
2020-12-16 16:22:34 -0500

    doodle jump bound checks refactor

--- projects/DoodleJump/js/bound_check.js
+++ projects/DoodleJump/js/bound_check.js
@@ -1,45 +1,13 @@
 class BoundCheck extends Canvas{
-    hitTop(y) {
-        if (y <= 0) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-
-    hitPaddle(block, paddle) {
-        if (block.x < paddle.x + paddle.width &&
-            block.x + block.width > paddle.x &&
-            block.y < paddle.y + paddle.height &&
-            block.y + block.height > paddle.y) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
     hitBottom(y, height) {
-        if (y + height >= this.canvasHeight) {
-            return true;
-        } else {
-            return false;
-        }
+        return y + height >= this.canvasHeight;
     }
 
     hitRight(x, width) {
-        if (x + width >= this.canvasWidth) {
-            return true;
-        } else {
-            return false;
-        }
+        return x + width >= this.canvasWidth;
     }
 
     hitLeft(x) {
-        if (x <= 0) {
-            return true;
-        } else {
-            return false;
-        }
+        return x <= 0;
     }
 }
