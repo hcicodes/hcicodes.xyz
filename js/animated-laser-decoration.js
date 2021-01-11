4304ab95a5433cf4151da7f35a0129109d61a18b
ChristianStefaniw <christian.stefaniw@student.tdsb.on.ca>
2021-01-11 17:57:04 -0500

    background animation and refactoring

---
+++ js/animated-laser-decoration.js
@@ -0,0 +1,21 @@
+function main(){
+    let pathEls = document.querySelectorAll("path");
+    for (let i = 0; i < pathEls.length; i++) {
+        let pathEl = pathEls[i];
+        let offset = anime.setDashoffset(pathEl);
+        pathEl.setAttribute("stroke-dashoffset", offset);
+        anime({
+            targets: pathEl,
+            strokeDashoffset: [offset, 0],
+            duration: anime.random(1000, 3000),
+            delay: anime.random(0, 2000),
+            loop: true,
+            direction: "alternate",
+            easing: "easeInOutSine",
+            autoplay: true
+        });
+    }
+
+}
+
+document.addEventListener("DOMContentLoaded", main)
